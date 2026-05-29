e2a391bb47474bed4e9c15f00a45887b2a2d3632
fz <296222957@qq.com>
2026-05-29 10:05:58 +0800

    adjust format time

--- src/main/java/com/tools/constant/Constants.java
+++ src/main/java/com/tools/constant/Constants.java
@@ -13,5 +13,5 @@ public class Constants {
     public static final String GROUP_ID = "GzipTools";
 
     public static final String CONTENT = "Content";
-    public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss: ";
+    public static final String TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 }
